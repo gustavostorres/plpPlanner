56c6bde107d44c68a16e51b25651931174a00465
Guilherme Silva <guilherme.dna06@gmail.com>
2022-05-06 02:47:56 -0300

    Criação de funções base para utilização de meta

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
+use App\Models\Meta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MetaController extends Controller
 {
-    //
+
+    public function index(){
+        $metas = Meta::where('user_id',Auth::user()->id)->get();
+
+        return view('metas.index')->with(['metas'=> $metas]);
+    }
+
+    public function create()
+    {
+        $categorias = Categoria::all();
+        return view('metas.new')->with(['categorias'=>$categorias]);
+    }
+
+    public function salvar(Request $request)
+    {
+        $meta = Meta::create(['statusMeta'=> 'indefinido', 'quantidadeTarefa'=>1,
+            'dataMeta'=>$request->dataMeta, 'nomeMeta'=>$request->nomeMeta,
+            'descricao'=>$request->descricao, 'categoria_id'=>$request->categoria_id,
+            'user_id'=>Auth::user()->id]);
+        $meta->save();
+        //$tarefas->metas()->sync([]);
+        return redirect(route('metas.index'));
+    }
+
+    public function edit($id)
+    {
+        $categorias = Categoria::all();
+        $meta = Meta::find($id);
+        return view('metas.update')->with(['categorias'=>$categorias, 'meta'=>$meta]);
+    }
+
+    public function update(Request $request)
+    {
+        $meta = Meta::find($request->meta_id);
+        $meta->nomeMeta = $request->nomeMeta;
+        $meta->categoria_id = $request->categoria_id;
+        $meta->descricao = $request->descricao;
+        $meta->dataMeta = $request->dataMeta;
+        $meta->statusMeta = $request->statusMeta;
+        $meta->update();
+        return redirect(route('metas.index'));
+    }
+
+    public function show($id)
+    {
+        $meta = Meta::find($id);
+        return view('metas.show')->with(['meta'=>$meta]);
+    }
+
 }
