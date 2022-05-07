7376794ae1cbed06ec7e3b864afca8273b8169c1
Guilherme Silva <guilherme.dna06@gmail.com>
2022-05-07 15:31:47 -0300

    Adicionado filtro para metas

--- resources/views/metas/index.blade.php
+++ resources/views/metas/index.blade.php
@@ -14,19 +14,43 @@
                 </div>
             </div>
             <br>
+            <div class="row">
+                <div class="col-md-4" style="float: left;">
+                    <select id="" class="form-control" onchange="exibirMetas(this)">
+                        <option value="all" selected>Todas</option>
+                        @foreach($categorias as $categoria)
+                            <option value="{{$categoria}}">{{$categoria}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-md-4" style="float: left;">
+                    <select id="" class="form-control" onchange="exibirMetas2(this)">
+                        <option value="all" selected>Todas</option>
+                        <option value="comSucesso">Sucesso</option>
+                        <option value="semSucesso">Sem Sucesso</option>
+                        <option value="parcialmenteAtingida">Parcialmente Atingida</option>
+                        <option value="indefinido">Indefinida</option>
+                    </select>
+                </div>
+            </div>
+
+
             @foreach($categorias as $categoria)
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg meta" id="{{$categoria}}">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <h5>
                             {{$categoria}}
                         </h5>
                         @foreach($metas as $meta)
                             @if($categoria == $meta->categoria->nomeCategoria)
-                                <li>
+                                <li style="margin-bottom: 10px" class="status" id="{{$meta->statusMeta}}">
                                     {{$meta->nomeMeta}} acontece entre {{$meta->dataMeta}} e {{$meta->dataFinalMeta}}
-                                    <a href="{{route('metas.edit', ['id' => $meta->id])}}" class="btn btn-primary">Editar meta</a>
-                                    <a href="{{route('metas.show', ['id' => $meta->id])}}" class="btn btn-primary">Mostrar detalhes</a>
-                                    <a href="{{route('metas.cadastrar.tarefa', ['id' => $meta->id])}}" class="btn btn-primary">Cad. Tarefa</a>
+                                    <a href="{{route('metas.edit', ['id' => $meta->id])}}" class="btn btn-primary">Editar
+                                        meta</a>
+                                    <a href="{{route('metas.show', ['id' => $meta->id])}}" class="btn btn-primary">Mostrar
+                                        detalhes</a>
+                                    <a href="{{route('metas.cadastrar.tarefa', ['id' => $meta->id])}}"
+                                       class="btn btn-primary">Cad. Tarefa</a>
                                     <a href="{{route('metas.destroy', ['id' => $meta->id])}}" class="btn btn-danger">Apagar</a>
                                 </li>
                             @endif
@@ -38,3 +62,27 @@
     </div>
 
 </x-app-layout>
+
+<script>
+    function exibirMetas(select) {
+        var metas = document.getElementsByClassName("meta");
+        for (var i = 0; i < metas.length; i++) {
+            if (select.value == metas[i].getAttribute("id") || select.value == "all") {
+                metas[i].style.display = "";
+            } else {
+                metas[i].style.display = "none";
+            }
+        }
+    }
+
+    function exibirMetas2(select) {
+        var metas = document.getElementsByClassName("status");
+        for (var i = 0; i < metas.length; i++) {
+            if (select.value == metas[i].getAttribute("id") || select.value == "all") {
+                metas[i].style.display = "";
+            } else {
+                metas[i].style.display = "none";
+            }
+        }
+    }
+</script>
