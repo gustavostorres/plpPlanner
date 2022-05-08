c7b91f29cc4dd27493d2ce3e14d7a866f6332fba
Guilherme Silva <guilherme.dna06@gmail.com>
2022-05-08 01:21:12 -0300

    Adicionado validações de tarefa e ajuste de tela de criação

--- resources/views/tarefas/new.blade.php
+++ resources/views/tarefas/new.blade.php
@@ -1,29 +1,99 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="col-md-6 mx-auto ">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <form method="POST" action="{{route('tarefas.salvar')}}">
-                      @csrf
-                      <label for="categoria_id">Categoria:</label><br>
-                      <select name="categoria_id" id="categoria_id">
-                          @foreach($categorias as $categoria)
-                          <option value="{{$categoria->id}}">{{$categoria->nomeCategoria}}</option>
-                          @endforeach
-                      </select><br>
-                      <label for="titulo">Titulo:</label><br>
-                      <input type="text" id="titulo" name="titulo"><br>
-                      <label for="nomeTarefa">Descrição:</label><br>
-                      <input type="text" id="nomeTarefa" name="nomeTarefa"><br>
-                      <label for="data">Data:</label><br>
-                      <input type="date" name="data"><br>
-                      <label for="horarioInicio">HorarioInicio:</label><br>
-                      <input type="time" name="horarioInicio"><br>
-                      <label for="horarioFim">HorarioFim:</label><br>
-                      <input type="time" name="horarioFim"><br>
-                      <input type="hidden" id="user_id" name="user_id" value="{{Auth::user()->id}}"><br>
-                      <a href="{{route('tarefas.index')}}" class="btn btn-primary" type="button">Voltar</a>
-                      <button class="btn btn-success" type="submit">Enviar</button><br>
+                        @csrf
+
+                        <div class="row justify-content-center">
+                            <div class="col-md-6">
+                                <label for="titulo">Titulo:</label><br>
+                                <input type="text" id="titulo" name="titulo"
+                                       class="form-control @error('titulo') is-invalid @enderror" value="{{old('titulo')}}">
+                                @error('titulo')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="categoria_id">Categoria:</label><br>
+                                <select name="categoria_id" id="categoria_id" class="form-control ">
+                                    @foreach($categorias as $categoria)
+                                        <option {{ old('categoria_id') == $categoria->id ? "selected" : "" }} value="{{$categoria->id}}">{{$categoria->nomeCategoria}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row justify-content-center">
+                            <div class="col-md-12">
+                                <label for="nomeTarefa">Descrição:</label><br>
+                                <textarea type="text" id="nomeTarefa" name="nomeTarefa"
+                                          class="form-control @error('nomeTarefa') is-invalid @enderror">{{old('nomeTarefa')}}</textarea>
+                                @error('nomeTarefa')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row justify-content-center">
+                            <div class="col-md-6">
+                                <label for="data">Data:</label><br>
+                                <input type="date" name="data"
+                                       class="form-control  @error('data') is-invalid @enderror" value="{{old('data')}}">
+                                @error('data')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="col-md-3">
+                                <label for="horarioInicio">Horario Inicio:</label><br>
+                                <input type="time" name="horarioInicio"
+                                       class="form-control  @error('horarioInicio') is-invalid @enderror" value="{{old('horarioInicio')}}">
+                                @error('horarioInicio')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-3">
+                                <label for="horarioFim">Horario Fim:</label><br>
+                                <input type="time" name="horarioFim"
+                                       class="form-control @error('horarioFim') is-invalid @enderror" value="{{old('horarioFim')}}">
+                                @error('horarioFim')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        @if(session('horario'))
+                            <br>
+                            <div class="row justify-content-center">
+                                <div class="col-sm-12 text-center">
+                                    <div class="alert alert-danger" role="alert">
+                                        <p>{{session('horario')}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                        <input type="hidden" id="user_id" name="user_id" value="{{Auth::user()->id}}"><br>
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 sm:text-left">
+                                <a href="{{route('tarefas.index')}}" class="btn btn-danger" type="button" style="width: 150px">Voltar</a>
+                            </div>
+                            <div class="col-md-6 sm:text-right">
+                                <button class="btn btn-success" type="submit" style="width: 150px">Criar</button>
+                            </div>
+                        </div>
+                        <br>
                     </form>
                 </div>
             </div>
