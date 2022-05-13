0399a0d3514ef6feb2b78595c2a192136d09bb77
Marcelino Barros <marcelino05052000@gmail.com>
2022-05-13 04:49:23 -0300

    Correção no show de tarefas e adição de cores nas paginas show e tarefasDia

--- resources/views/metas/metasDia.blade.php
+++ resources/views/metas/metasDia.blade.php
@@ -12,18 +12,41 @@
             <br>
             @foreach($categorias as $categoria)
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
+                    @if($categoria->nomeCategoria == 'Saúde')
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};">
+                    @elseif($categoria->nomeCategoria == 'Esportes') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};">
+                    @elseif($categoria->nomeCategoria == 'Lazer') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};">
+                    @elseif($categoria->nomeCategoria == 'Estudo') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};">
+                    @elseif($categoria->nomeCategoria == 'Trabalho') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};"> 
+                    @elseif($categoria->nomeCategoria == 'Ligações importantes') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};">
+                    @elseif($categoria->nomeCategoria == 'Reuniões') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};"> 
+                    @else <div class="p-6 border-b border-gray-200" style="background-color: {{$categoria->cor}};"> 
+                    @endif
                         <h5>
-                            {{$categoria}}
+                            {{$categoria->nomeCategoria}}
                         </h5>
                         @foreach($metas as $meta)
-                            @if($categoria == $meta->categoria->nomeCategoria)
-                                <li>
-                                    {{$meta->nomeMeta}}, com o status: Status: {{$meta->statusMeta}}
-                                    <a href="{{route('metas.edit', ['id' => $meta->id])}}" class="btn btn-primary">Editar meta</a>
-                                    <a href="{{route('metas.show', ['id' => $meta->id])}}" class="btn btn-primary">Mostrar detalhes</a>
-                                    <a href="{{route('metas.destroy', ['id' => $meta->id])}}" class="btn btn-danger">Apagar</a>
+                            @if($categoria->nomeCategoria == $meta->categoria->nomeCategoria)
+                                <ul class="status" id="{{$meta->statusMeta}}">
+                                <li><b>Nome:</b> {{$meta->nomeMeta}}
+                                <ul>
+                                    <div style="float: right">
+                                        <a href="{{route('metas.edit', ['id' => $meta->id])}}" class="btn btn-primary">Editar meta</a>
+                                        <a href="{{route('metas.show', ['id' => $meta->id])}}" class="btn btn-primary">Ver detalhes</a>
+                                        <a href="{{route('metas.cadastrar.tarefa', ['id' => $meta->id])}}" class="btn btn-primary">Cadastrar Tarefa</a>
+                                        <a href="{{route('metas.destroy', ['id' => $meta->id])}}" class="btn btn-danger">Apagar</a>
+                                    </div>
+                                    <li><b>Acontece entre:</b> {{$meta->dataMeta}} e {{$meta->dataFinalMeta}}</li>
+                                    <li><b>Status:</b> {{$meta->statusMeta}}</li>
+                                </ul>
                                 </li>
+                            </ul>
                             @endif
                         @endforeach
                     </div>
--- resources/views/metas/show.blade.php
+++ resources/views/metas/show.blade.php
@@ -2,7 +2,22 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                @if($meta->categoria->nomeCategoria == 'Saúde')
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};">
+                    @elseif($meta->categoria->nomeCategoria == 'Esportes') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};">
+                    @elseif($meta->categoria->nomeCategoria == 'Lazer') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};">
+                    @elseif($meta->categoria->nomeCategoria == 'Estudo') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};">
+                    @elseif($meta->categoria->nomeCategoria == 'Trabalho') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};"> 
+                    @elseif($meta->categoria->nomeCategoria == 'Ligações importantes') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};">
+                    @elseif($meta->categoria->nomeCategoria == 'Reuniões') 
+                    <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};"> 
+                    @else <div class="p-6 border-b border-gray-200" style="background-color: {{$meta->categoria->cor}};"> 
+                    @endif
                     <label for="categoria">Categoria: {{$meta->categoria->nomeCategoria}} </label><br>
                     <label for="nome">Nome: {{$meta->nomeMeta}} </label><br>
                     <label for="nomeTarefa">Descrição: {{$meta->descricao}} </label><br>
