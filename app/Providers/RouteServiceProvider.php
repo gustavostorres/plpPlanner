d418719f9c69fa28a8740671dc5544d514decb72
Marcelino Barros <marcelino05052000@gmail.com>
2022-05-13 04:44:45 -0300

    Mudando a rota após o login para o planner

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/tarefas/index';
+    public const HOME = '/planner';
 
     /**
      * The controller namespace for the application.
