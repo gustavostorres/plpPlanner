8113365fd0879d670bafa2d467e7cdaf270f8b67
Guilherme Silva <guilherme.dna06@gmail.com>
2022-05-15 01:50:24 -0300

    Criada lógica para geração de relatório de semanas e meses mais produtivos

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -20,33 +20,41 @@ class RelatorioController extends Controller
 
     public function gerar(Request $request)
     {
-        //$dataInicial = new DateTime($request->dataInicial);
-        //$dataFinal = new DateTime($request->dataFinal);
-        //Categorias de metas mais realizadas
         $validatedTime = $request->validate([
-            'dataInicial' => ['required','date'],
-            'dataFinal' => ['required','date','after_or_equal:dataInicial'],
+            'dataInicial' => ['required', 'date'],
+            'dataFinal' => ['required', 'date', 'after_or_equal:dataInicial'],
             'tipo' => ['required'],
         ]);
         if ($request->tipo == 1 || $request->tipo == 4) {
             return $this->categoriaMetasRealizadas($request);
         } elseif ($request->tipo == 2 || $request->tipo == 5) {
             return $this->categoriaTarefasRealizadas($request);
-        } else if($request->tipo == 3){
+        } else if ($request->tipo == 3) {
             return $this->turnoProdutivo($request);
+        } elseif ($request->tipo == 6) {
+            return $this->periodoMaisProdutivo($request);
         }
 
     }
 
-    function aksort(&$array,$valrev=false,$keyrev=false) {
-        if ($valrev) { arsort($array); } else { asort($array); }
+    function aksort(&$array, $valrev = false, $keyrev = false)
+    {
+        if ($valrev) {
+            arsort($array);
+        } else {
+            asort($array);
+        }
         $vals = array_count_values($array);
         $i = 0;
-        foreach ($vals AS $val=>$num) {
-            $first = array_splice($array,0,$i);
-            $tmp = array_splice($array,0,$num);
-            if ($keyrev) { krsort($tmp); } else { ksort($tmp); }
-            $array = array_merge($first,$tmp,$array);
+        foreach ($vals as $val => $num) {
+            $first = array_splice($array, 0, $i);
+            $tmp = array_splice($array, 0, $num);
+            if ($keyrev) {
+                krsort($tmp);
+            } else {
+                ksort($tmp);
+            }
+            $array = array_merge($first, $tmp, $array);
             unset($tmp);
             $i = $num;
         }
@@ -62,15 +70,14 @@ class RelatorioController extends Controller
             $categoria->porcentagem = ($categoria->quantidade / $quantGeral) * 100;
         }
 
-        $categorias = Arr::sort($categorias, function($categoria)
-        {
+        $categorias = Arr::sort($categorias, function ($categoria) {
             return $categoria->quantidade;
         });
 
         if ($request->tipo == 1) {
-            return view('relatorio.categoriaMetaRealizada')->with(['categorias' => $categorias, 'metas' => $metas->get(),'dataInicial'=>$request->dataInicial,'dataFinal'=>$request->dataFinal,'tipo' =>$request->tipo]);
+            return view('relatorio.categoriaMetaRealizada')->with(['categorias' => $categorias, 'metas' => $metas->get(), 'dataInicial' => $request->dataInicial, 'dataFinal' => $request->dataFinal, 'tipo' => $request->tipo]);
         }
-        return view('relatorio.quantMetaCumprida')->with(['categorias' => $categorias, 'metas' => $metas->get(),'dataInicial'=>$request->dataInicial,'dataFinal'=>$request->dataFinal,'tipo' =>$request->tipo]);
+        return view('relatorio.quantMetaCumprida')->with(['categorias' => $categorias, 'metas' => $metas->get(), 'dataInicial' => $request->dataInicial, 'dataFinal' => $request->dataFinal, 'tipo' => $request->tipo]);
     }
 
     public function categoriaTarefasRealizadas(Request $request)
@@ -80,23 +87,19 @@ class RelatorioController extends Controller
         $quantGeral = Tarefa::all()->count();
         foreach ($categorias as $categoria) {
             $categoria->quantidade = Tarefa::where("user_id", Auth::user()->id)->where("statusTarefa", "executada")->whereDate('data', '>=', $request->dataInicial)->whereDate('data', '<=', $request->dataFinal)->where("categoria_id", $categoria->id)->get()->count();
-            $categoria->porcentagem = round((($categoria->quantidade / $quantGeral) * 100),2);
+            $categoria->porcentagem = round((($categoria->quantidade / $quantGeral) * 100), 2);
         }
-        $categorias = Arr::sort($categorias, function($categoria)
-        {
+        $categorias = Arr::sort($categorias, function ($categoria) {
             return $categoria->quantidade;
         });
         if ($request->tipo == 2) {
-            return view('relatorio.categoriaTarefaRealizada')->with(['categorias' => $categorias, 'tarefas' => $tarefas->get(),'dataInicial'=>$request->dataInicial,'dataFinal'=>$request->dataFinal,'tipo' =>$request->tipo]);
+            return view('relatorio.categoriaTarefaRealizada')->with(['categorias' => $categorias, 'tarefas' => $tarefas->get(), 'dataInicial' => $request->dataInicial, 'dataFinal' => $request->dataFinal, 'tipo' => $request->tipo]);
         }
-        return view('relatorio.quantTarefaCumprida')->with(['categorias' => $categorias, 'tarefas' => $tarefas->get(),'dataInicial'=>$request->dataInicial,'dataFinal'=>$request->dataFinal,'tipo' =>$request->tipo]);
+        return view('relatorio.quantTarefaCumprida')->with(['categorias' => $categorias, 'tarefas' => $tarefas->get(), 'dataInicial' => $request->dataInicial, 'dataFinal' => $request->dataFinal, 'tipo' => $request->tipo]);
     }
 
-    public function mesesProdutivos(Request $request){
-
-    }
-
-    public function turnoProdutivo(Request $request){
+    public function turnoProdutivo(Request $request)
+    {
         $tarefas = Tarefa::where("user_id", Auth::user()->id)->where("statusTarefa", "executada")->whereDate('data', '>=', $request->dataInicial)->whereDate('data', '<=', $request->dataFinal);
         $turnos = $tarefas->select('turno')->distinct()->get();
 
@@ -104,10 +107,80 @@ class RelatorioController extends Controller
             $turno->quantidade = Tarefa::where("user_id", Auth::user()->id)->where("statusTarefa", "executada")->whereDate('data', '>=', $request->dataInicial)->whereDate('data', '<=', $request->dataFinal)->where('turno', $turno->turno)->get()->count();
         }
 
-        $turnos = Arr::sort($turnos, function($turno)
-        {
+        $turnos = Arr::sort($turnos, function ($turno) {
             return $turno->quantidade;
         });
-        return view('relatorio.turnoProdutivo')->with(['turnos' => $turnos, 'tarefas' => $tarefas->get(),'dataInicial'=>$request->dataInicial,'dataFinal'=>$request->dataFinal,'tipo' =>$request->tipo]);
+        return view('relatorio.turnoProdutivo')->with(['turnos' => $turnos, 'tarefas' => $tarefas->get(), 'dataInicial' => $request->dataInicial, 'dataFinal' => $request->dataFinal, 'tipo' => $request->tipo]);
     }
+
+    public function gerarAnos(Request $request)
+    {
+        $inicio = new \DateTime($request->dataInicial);
+        $fim = new \DateTime($request->dataFinal);
+        $anos = [];
+        while ($inicio->format('Y') <= $fim->format('Y')) {
+            $aux = new \stdClass();
+            $aux->ano = $inicio->format('Y');
+            array_push($anos, $aux);
+            $inicio->modify('+1 year');
+        }
+        return $anos;
+    }
+
+    public function gerarMeses(Request $request, $anosTemp)
+    {
+        $inicio = new \DateTime($request->dataInicial);
+        $fim = new \DateTime($request->dataFinal);
+        $hoje = new \DateTime();
+
+        foreach ($anosTemp as $anoTemp) {
+            $meses = [];
+            while (($inicio->format('m') <= 12 && $anoTemp->ano == $inicio->format('Y')) &&
+                ($inicio->format('Y-m') <= $fim->format('Y-m'))) {
+                $aux = new \stdClass();
+                $aux->mes = $inicio->format('m');
+                $aux->semanas = [];
+                array_push($meses, $aux);
+                $inicio->modify('+1 month');
+            }
+            $anoTemp->meses = $meses;
+        }
+    }
+
+    public function gerarSemanas(Request $request,$data, $tarefas)
+    {
+        foreach ($data->meses as $mes) {
+            $dataInicio = new DateTime('01-' . $mes->mes . '-' . $data->ano);
+            $mesAux = $dataInicio->format('m');
+            $contSemana = 1;
+
+            while ($mesAux == $dataInicio->format('m')) {
+                $cont = 0;
+                for ($i = $dataInicio->format('w'); $i <= 6 && $mesAux == $dataInicio->format('m'); $i++) {
+                    if (Tarefa::where("user_id", Auth::user()->id)->where("statusTarefa", "executada")->whereDate('data', '>=', $request->dataInicial)->whereDate('data', '<=', $request->dataFinal)->where('data', $dataInicio)->get()->count() > 0){
+                        $cont++;
+                    }
+                    $dataInicio->modify('+1 day');
+                }
+                $aux = new \stdClass();
+                $aux->semana = $contSemana;
+                $aux->quantidade = $cont;
+                if($cont>0){
+                    array_push($mes->semanas,$aux);
+                }
+                $contSemana++;
+            }
+        }
+    }
+
+    public function periodoMaisProdutivo(Request $request)
+    {
+        $tarefas = Tarefa::where("user_id", Auth::user()->id)->where("statusTarefa", "executada")->whereDate('data', '>=', $request->dataInicial)->whereDate('data', '<=', $request->dataFinal);
+        $anos = $this->gerarAnos($request);
+        $this->gerarMeses($request, $anos);
+        foreach ($anos as $ano) {
+            $this->gerarSemanas($request,$ano, $tarefas);
+        }
+    }
+
 }
